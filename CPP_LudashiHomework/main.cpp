58194d23c9906d849240148ccc54354330b3e42e
LexTheAries2209 <673661658@qq.com>
2023-10-19 13:53:42 +0800

    Initial Commit

---
+++ CPP_LudashiHomework/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP_LudashiHomework
+//
+//  Created by 吴坤城 on 10/19/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
