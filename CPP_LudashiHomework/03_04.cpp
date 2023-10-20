782989554f873cd26faebf333537203ebfafbf8b
LexTheAries2209 <673661658@qq.com>
2023-10-20 19:38:50 +0800

    1020总结

--- CPP_LudashiHomework/03_04.cpp
+++ CPP_LudashiHomework/03_04.cpp
@@ -5,4 +5,24 @@
 //  Created by 吴坤城 on 10/20/23.
 //
 
-#include <stdio.h>
+//#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+//int  main() {
+//    int a;
+//    cout << "请输入一个十以内行数：" ;
+//    cin >> a ;
+//    if (a>10) {
+//        cout << "你输入的行数太大了！" << endl;
+//    }
+//    else {
+//        do {
+//            for (int i=0; i<5; i++) {
+//                cout << "" ;
+//            }
+//        } while ();
+//    }
+//    
+//    
+//    return 0;
+//}
