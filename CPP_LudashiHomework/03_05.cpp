eaaa27c29283e9c895da12cff0f0c14b81b4fb6e
LexTheAries2209 <673661658@qq.com>
2023-10-21 01:45:19 +0800

    完成了作业0305

---
+++ CPP_LudashiHomework/03_05.cpp
@@ -0,0 +1,34 @@
+//
+//  03_05.cpp
+//  CPP_LudashiHomework
+//
+//  Created by 吴坤城 on 10/21/23.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int  main() {
+    int a,b;
+    cout << "请输入两个正整数作为范围，用空格间隔：";
+    cin >> a >> b;
+    if ((b <= a)||(a < 0)||(b < 0)) {
+        cout << "你输入了无法计算的两个值，请重新输入。";
+    } else {
+        for(int i=a; i<=b; i++) {
+            int k = 1;
+            for(int j=2; j<=sqrt(i); j++) {
+                if(i % j == 0) {
+                    k = 0;
+                    break;
+                }
+            }
+            if(k == 1)
+                cout << i << " ";
+        }
+        
+        
+    }
+    return 0;
+}
