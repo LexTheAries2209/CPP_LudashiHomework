8cd018279669b145c5ad248d2fafefcec80049d5
LexTheAries2209 <673661658@qq.com>
2023-11-03 11:58:39 +0800

    完善了作业2753

--- CPP_LudashiHomework/OpenJudge2753.cpp
+++ CPP_LudashiHomework/OpenJudge2753.cpp
@@ -17,11 +17,21 @@ int main() {
     int n;
     cout << "请输入要测试的行数n和要测试的第a项，以换行间隔：" << endl;
     cin >> n;
+    
+    if(cin.fail() || n <= 0) {
+        cout << "n必须为正整数！" << endl;
+        return 0;
+      }
+    
     int results[n];
   
     for(int i = 0; i < n; i++) {
         int a;
         cin >> a;
+        if(cin.fail() || a <= 0) {
+            cout << "a必须为正整数！" << endl;
+            return 0;
+          }
         results[i] = func(a);
     }
     
