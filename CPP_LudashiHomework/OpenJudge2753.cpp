7b0a06b7cc6069a979b81817bd4ee83cde8ea028
LexTheAries2209 <673661658@qq.com>
2023-11-03 10:36:30 +0800

    Finish homework openjudge 4147

---
+++ CPP_LudashiHomework/OpenJudge2753.cpp
@@ -0,0 +1,8 @@
+//
+//  OpenJudge2753.cpp
+//  CPP_LudashiHomework
+//
+//  Created by 吴坤城 on 11/3/23.
+//
+
+#include <stdio.h>
---
+++ CPP_LudashiHomework/OpenJudge4147.cpp
@@ -0,0 +1,32 @@
+//
+//  OpenJudge4147.cpp
+//  CPP_LudashiHomework
+//
+//  Created by 吴坤城 on 11/3/23.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void func(int n, char a, char b, char c) {
+  if(n == 1) {
+    cout << n << ":" << a << "->" << c << endl;
+  } else {
+    func(n-1, a, c, b);
+    cout << n << ":" << a << "->" << c << endl;
+    func(n-1, b, a, c);
+  }
+}
+
+int main() {
+  int n;
+  string a, b, c;
+    cout << "请依次输入圆盘数量和三个柱子的名称，以空格间隔：" ;
+  cin >> n >> a >> b >> c;
+
+  func(n, a[0], b[0], c[0]);
+  
+  return 0;
+}
