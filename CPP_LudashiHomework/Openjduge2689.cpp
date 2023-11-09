0b80724b3656fb2766f30cde53113facf2980e34
LexTheAries2209 <673661658@qq.com>
2023-11-09 16:51:00 +0800

    完成了作业2689

---
+++ CPP_LudashiHomework/Openjduge2689.cpp
@@ -0,0 +1,34 @@
+//
+//  Openjduge2689.cpp
+//  CPP_LudashiHomework
+//
+//  Created by 吴坤城 on 11/9/23.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Func(const string& str) {
+    string result = str;
+
+    for (char& c : result) {
+        if (c >= 'A' && c <= 'Z') {
+            c += 32;
+        } else if (c >= 'a' && c <= 'z') {
+            c -= 32;
+        }
+    }
+    return result;
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    
+    string Output = Func(str);
+    
+    cout << Output << endl;
+    
+    return 0;
+}
