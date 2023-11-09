1fa666948be754e9d5f5be4123047919b9bb1dac
LexTheAries2209 <673661658@qq.com>
2023-11-09 16:13:52 +0800

    完善了作业2739

--- CPP_LudashiHomework/OpenJudge2739.cpp
+++ CPP_LudashiHomework/OpenJudge2739.cpp
@@ -5,4 +5,23 @@
 //  Created by 吴坤城 on 11/9/23.
 //
 
-#include "OpenJudge2739.hpp"
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cout << "请输入底数a和指数b，以空格间隔：";
+    cin >> a >> b;
+    
+    int a1 = stoi(a);
+    int b1 = stoi(b);
+    
+    int x = log(b1) / log(a1);  // 计算x
+    
+    cout << x << endl;
+    
+    return 0;
+}
+
