024d6a2670363bc684abde47eba8acc05b88aea8
LexTheAries2209 <673661658@qq.com>
2023-11-10 10:50:27 +0800

    完成了作业2746

--- CPP_LudashiHomework/OpenJudge2746.cpp
+++ CPP_LudashiHomework/OpenJudge2746.cpp
@@ -5,4 +5,22 @@
 //  Created by 吴坤城 on 11/10/23.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int func(int n, int m) {
+    int King = 0;
+    for (int i = 2; i <= n; i++) {
+        King = (King + m) % i;
+    }
+    return King + 1;
+}
+
+int main() {
+    int n, m;
+    while (cin >> n >> m && !(n == 0 && m == 0)) {
+        int MonkeyKing = func(n, m);
+        cout << MonkeyKing << endl;
+    }
+    return 0;
+}
