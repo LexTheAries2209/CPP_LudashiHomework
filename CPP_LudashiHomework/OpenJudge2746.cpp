c52db1484b74922d858b5363eea9be2fe8227206
LexTheAries2209 <673661658@qq.com>
2023-11-10 10:52:03 +0800

    提交2746

--- CPP_LudashiHomework/OpenJudge2746.cpp
+++ CPP_LudashiHomework/OpenJudge2746.cpp
@@ -18,6 +18,7 @@ int func(int n, int m) {
 
 int main() {
     int n, m;
+    
     while (cin >> n >> m && !(n == 0 && m == 0)) {
         int MonkeyKing = func(n, m);
         cout << MonkeyKing << endl;
